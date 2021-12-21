c0065978187ef7170c37d9c65985d04a15827e97
luisbryandr <luisbdominguezr@gmail.com>
2021-12-21 16:50:32 -0500

    ALL Done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,109 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//nav-Bar [done]
+const nav = document.querySelector('nav');
+let aTag1 = nav.querySelector('a:nth-of-type(1)');
+let aTag2 = nav.querySelector('a:nth-of-type(2)');
+let aTag3 = nav.querySelector('a:nth-of-type(3)');
+let aTag4 = nav.querySelector('a:nth-of-type(4)');
+let aTag5 = nav.querySelector('a:nth-of-type(5)');
+let aTag6 = nav.querySelector('a:nth-of-type(6)');
+
+aTag1.textContent = siteContent['nav']['nav-item-1']
+aTag2.textContent = siteContent['nav']['nav-item-2']
+aTag3.textContent = siteContent['nav']['nav-item-3']
+aTag4.textContent = siteContent['nav']['nav-item-4']
+aTag5.textContent = siteContent['nav']['nav-item-5']
+aTag6.textContent = siteContent['nav']['nav-item-6']
+
+//cta - [done -minus br inside h1]
+let cta = document.querySelector('.cta');
+let title = cta.querySelector('h1');
+title.textContent = siteContent['cta']['h1'];
+// let br = document.createElement('br');
+// title.appendChild(br);  circling back to do the br
+// console.log(title);
+let ctaButton = cta.querySelector('button');
+ctaButton.textContent = siteContent['cta']['button'];
+
+let ctaImage = cta.querySelector('img');
+ctaImage.setAttribute('src', siteContent['cta']['img-src']);
+
+//main-content -- []
+let mainContent = document.querySelector('.main-content');
+
+let subHeading1 = mainContent.querySelector('h4:nth-of-type(1)');
+let p1 = mainContent.querySelector('p:nth-of-type(1)')
+subHeading1.textContent = siteContent['main-content']['features-h4'];
+p1.textContent = siteContent['main-content']['features-content'];
+
+// let subHeading2 = subHeading1.nextElementSibling;
+// let p2 = mainContent.querySelector('p:nth-of-type(2)');
+// console.log(subHeading2)
+// subHeading2.textContent = siteContent['main-content']['about-content']
+// let textContent = document.querySelectorAll('.text-content');
+// const textContentArray = Array.from(textContent)
+// console.log(textContentArray)
+
+// console.log(textContentArray[1].nextElementSibling);//null
+
+let subHeadingsAll = document.querySelectorAll('h4');
+const subHeadsArray = Array.from(subHeadingsAll)
+//Services - I could store services reference inside a variable and do the same for each sub heading and their paragraph but I'm juust going to save my coding fingers atm and just get the job done
+// i would that by this const servicesHeading = subHeadsArray[1]
+//then just use servicesHeading.textContent & .nextElementSibling to render the contnetn I want
+
+subHeadsArray[1].textContent = siteContent['main-content']['about-h4']
+subHeadsArray[1].nextElementSibling.textContent = siteContent['main-content']['about-content'];
+
+subHeadsArray[2].textContent = siteContent['main-content']['services-h4'];
+subHeadsArray[2].nextElementSibling.textContent= siteContent['main-content']['services-content']
+
+subHeadsArray[3].textContent = siteContent['main-content']['product-h4'];
+subHeadsArray[3].nextElementSibling.textContent= siteContent['main-content']['product-content']
+
+
+subHeadsArray[4].textContent = siteContent['main-content']['vision-h4'];
+subHeadsArray[4].nextElementSibling.textContent= siteContent['main-content']['vision-content']
+
+let middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+
+//contact
+let contact = document.querySelector('.contact');
+
+let contactHeading = contact.querySelector('h4');
+contactHeading.textContent = siteContent['contact']['contact-h4'];
+
+let address = contactHeading.nextElementSibling;
+address.textContent = siteContent['contact']['address'];
+
+let phone = address.nextElementSibling;
+phone.textContent = siteContent['contact']['phone'];
+
+let email = phone.nextElementSibling;
+email.textContent = siteContent['contact']['email'];
+
+//footer
+let footer = document.querySelector('footer');
+let copyright = footer.querySelector('p');
+copyright = siteContent['footer']['copyright'];
+
+
+
+const dataLink = document.createElement('a');
+dataLink.textContent = 'Data';
+dataLink.href = '#'
+
+nav.prepend(dataLink)
+
+const awesomeLink = document.createElement('a');
+awesomeLink.textContent = 'Awesomeness';
+dataLink.href = '#'
+
+nav.appendChild(awesomeLink)
+
+nav.style.color = 'green';
